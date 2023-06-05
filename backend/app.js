618159aff0350ee229575520845e83ec763ab195
rosept <124017598+rosept@users.noreply.github.com>
2023-06-05 06:52:01 +0000

    integrated backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,19 @@ import blogRouter from "./routes/blog-routes.js";
 import router from './routes/user-routes.js';
 import cors from 'cors';
 import logger from 'morgan';
+import path from "path";
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
+app.get("*", (req,res,next) => {
+    res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+})
 
 mongoose
 
